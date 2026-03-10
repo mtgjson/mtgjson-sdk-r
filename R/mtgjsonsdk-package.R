01b3302e1b99516127362e5a2f39ef206f508ef7
ZeldaZach <zahalpern+github@gmail.com>
2026-03-09 23:02:44 -0400

    package details

---
+++ R/mtgjsonsdk-package.R
@@ -0,0 +1,7 @@
+#' @keywords internal
+#' @importFrom R6 R6Class
+#' @importFrom DBI dbConnect dbDisconnect dbExecute dbGetQuery dbWriteTable
+#' @importFrom duckdb duckdb
+#' @importFrom httr2 request req_timeout req_perform resp_body_json req_progress
+#' @importFrom jsonlite fromJSON write_json
+"_PACKAGE"
